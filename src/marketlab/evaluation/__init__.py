17de9d21dcab4776c5e61ea4f712a1bc7f87b6df
Ricardo García Ramírez <rgr.5882@gmail.com>
2026-03-27 13:52:10 -0600

    feat: add walk-forward fold generator

---
+++ src/marketlab/evaluation/__init__.py
@@ -0,0 +1,11 @@
+from .walk_forward import (
+    WalkForwardFold,
+    build_walk_forward_folds,
+    slice_fold_rows,
+)
+
+__all__ = [
+    "WalkForwardFold",
+    "build_walk_forward_folds",
+    "slice_fold_rows",
+]
